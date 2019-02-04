f18013bbee227ec7bb7178fdfbe289188e33fd65
kyamamot <keisuke.yamamoto.rg@hitachi.com>
2019-02-04 18:20:42 +0900

    update

--- eval_sambamba_sort/run_all.sh
+++ eval_sambamba_sort/run_all.sh
@@ -4,15 +4,15 @@ task_name=eval_sambamba_sort
 docker_image=kyamamot/eval_sambamba-0.6.8:20190201
 aws_ec2_instance_type=m5.4xlarge
 aws_disk_size=120
-aws_s3_backet=s3://keisuke-singapore/${task_name}/
+aws_s3_bucket=s3://keisuke-singapore/${task_name}/
 
 
 root_directory=$(cd $(dirname ${0}) && pwd)
 
 
-/bin/bash ../src/_run_all.sh "${task_name}" \
-                             "${docker_image}" \
-                             "${aws_ec2_instance_type}" \
-                             "${aws_disk_size}" \
-                             "${aws_s3_backet}" \
-                             "${root_directory}"
+/bin/bash -x  ../src/_run_all.sh "${task_name}" \
+                                 "${docker_image}" \
+                                 "${aws_ec2_instance_type}" \
+                                 "${aws_disk_size}" \
+                                 "${aws_s3_bucket}" \
+                                 "${root_directory}"

5b21c40b3535780a9c5a2e21999752c773766b24
kyamamot <keisuke.yamamoto.rg@hitachi.com>
2019-02-08 12:40:20 +0900

    update

---
+++ eval_bwa_thread/README.md
@@ -0,0 +1,15 @@
+# eval_bwa_thread
+
+m5.4xlarge spot instance - 16 cpu, 64GiB memory, 80GB EBS
+
+## Running Time
+![Running Time](output/running_time.png)
+
+## CPU Utilization
+![CPU Utilization](output/cpu_utilization.png)
+
+## Memory Utilization
+![Memory Utilization](output/memory_utilization.png)
+
+## Disk Storage Usage
+![Disk Storage Usage](output/disk_storage_usage.png)
--- eval_bwa_thread/run_all.sh
+++ eval_bwa_thread/run_all.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
 task_name=eval_bwa_thread
-docker_image=kyamamot/eval_bwa-0.7.17:20190204
-aws_ec2_instance_type=m5.4xlarge
-aws_disk_size=80
-aws_s3_bucket=s3://keisuke-singapore/${task_name}/
-
 
 root_directory=$(cd $(dirname ${0}) && pwd)
+config_file=${root_directory}/run_config.txt
+if [ ! -f ${config_file} ]; then
+	exit 1
+fi
 
+source ${config_file}
+
+task_template_file=${root_directory}/ecsub/tasks-${task_name}.tsv.template
+if [ ! -f ${task_template_file} ]; then
+	exit 1
+fi
+
+task_file=${task_template_file%.template}
+
+cat ${task_template_file} | sed "s|___SAMPLE___|${SAMPLE}|g" \
+                          | sed "s|___INPUT_FASTQ1___|${INPUT_FILE}|g" \
+                          | sed "s|___INPUT_FASTQ2___|${INPUT_FILE}|g" \
+                          | sed "s|___OUTPUT_DIR___|${OUTPUT_DIR}|g" \
+                          | sed "s|___REFERENCE___|${REFERENCE}|g" \
+                          > ${task_file}
 
 /bin/bash -x ../src/_run_all.sh "${task_name}" \
-                                "${docker_image}" \
-                                "${aws_ec2_instance_type}" \
-                                "${aws_disk_size}" \
-                                "${aws_s3_bucket}" \
-                                "${root_directory}"
+                                "${task_file}" \
+                                "${root_directory}" \
+                                "${DOCKER_IMAGE}" \
+                                "${AWS_EC2_INSTANCE_TYPE}" \
+                                "${AWS_DISK_SIZE}" \
+                                "${OUTPUT_DIR}" \
+                                "${EVAL_DESCRIPTION}"

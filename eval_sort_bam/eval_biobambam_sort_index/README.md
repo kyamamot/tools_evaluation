f07e120518bfa458c88fc73143156fb7f86b4950
kyamamot <keisuke.yamamoto.rg@hitachi.com>
2019-02-14 19:22:56 +0900

    re-create chart

---
+++ eval_sort_bam/eval_biobambam_sort_index/README.md
@@ -0,0 +1,19 @@
+# eval_biobambam_sort_index
+
+## Description
+bamsort [inputthreads={n} outputthreads={m}] inputformat={sam|bam} outputformat=bam I=input.{sam|bam} O=output.bam --tmpfile=/scratch
+
+## Computing Environment
+aws m5.4xlarge EC2 spot instance - 16 cpu, 64GiB memory, 120GB EBS
+
+## Running Time
+![Running Time](output/running_time.png)
+
+## CPU Utilization
+![CPU Utilization](output/cpu_utilization.png)
+
+## Memory Utilization
+![Memory Utilization](output/memory_utilization.png)
+
+## Disk Storage Usage
+![Disk Storage Usage](output/disk_storage_usage.png)
--- src/R/plot.R
+++ src/R/plot.R
@@ -29,6 +29,7 @@ chart_runtime <- ggplot(count_data, aes(x = V1, y = freq, fill = factor(V1))) +
 output_file <- paste(output_directory, "running_time.png", sep = "/")
 ggsave(file = output_file, chart_runtime)
 
+conditions_order <- unique(data$V1)
 data$V1 <- factor(data$V1, levels=conditions_order)
 
 chart_cpu <- ggplot(data, aes(x = data[,2], y = data[,4])) +
@@ -44,6 +45,9 @@ ggsave(file = output_file, chart_cpu)
 input_file <- paste(metrics_directory, "MemoryUtilization.txt", sep = "/")
 data <- read.csv(input_file, header = F, sep = "\t")
 
+conditions_order <- unique(data$V1)
+data$V1 <- factor(data$V1, levels=conditions_order)
+
 chart_memory <- ggplot(data, aes(x = data[,2], y = data[,4])) +
     geom_line(aes(color = factor(data[,1]))) +
     ylim(0,100) +
@@ -56,6 +60,9 @@ ggsave(file = output_file, chart_memory)
 # Data Storage Utilization
 input_file <- paste(metrics_directory, "DataStorageUtilization.txt", sep = "/")
 data <- read.csv(input_file, header = F, sep = "\t")
+
+conditions_order <- unique(data$V1)
+data$V1 <- factor(data$V1, levels=conditions_order)
 
 chart_storage <- ggplot(data, aes(x = data[,2], y = data[,4])) +
     geom_line(aes(color = factor(data[,1]))) +

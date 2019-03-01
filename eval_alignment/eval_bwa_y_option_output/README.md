eec0bcdb8d575668acced36d1d7b166057cc2fd6
kyamamot <keisuke.yamamoto.rg@hitachi.com>
2019-03-01 14:14:46 +0900

    update

---
+++ eval_alignment/eval_bwa_y_option_output/README.md
@@ -0,0 +1,19 @@
+# eval_bwa_y_option_output
+
+## Description
+bwa mem -K 10000000 [-Y] -t 16 -T 0
+
+## Computing Environment
+m5.4xlarge Amazon EC2 spot instance - 16 cpu, 64GiB memory, 80GB EBS
+
+## Running Time
+![Running Time](output/running_time.png)
+
+## CPU Utilization
+![CPU Utilization](output/cpu_utilization.png)
+
+## Memory Utilization
+![Memory Utilization](output/memory_utilization.png)
+
+## Disk Storage Usage
+![Disk Storage Usage](output/disk_storage_usage.png)
---
+++ eval_alignment_combination/eval_combination_01/ecsub/script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+REFERENCE=${REFERENCE}/GRCh38.d1.vd1.fa
+
+mkdir -p ${OUTPUT_DIR}
+
+output_file=${OUTPUT_DIR}/${SAMPLE}.uncompressed.name-sorted.bam
+
+read_group='@RG\tID:12345\tPL:illumina\tLB:lib\tSM:5929_tumor'
+
+if [ "${TOOL_COMBINATION}" == "1" ]; then
+    # all samtools
+    time /tools/bwa-0.7.17/bwa mem ${BWA_OPTION} -R "${read_group}" ${REFERENCE} ${INPUT_FASTQ1} ${INPUT_FASTQ2} \
+        | /tools/samtools-1.9/samtools view ${SAMTOOLS_VIEW_OPTION} \
+        | /tools/samtools-1.9/samtools sort ${SAMTOOLS_SORT_OPTION} -o ${output_file}
+
+
+elif [ "${TOOL_COMBINATION}" == "2" ]; then
+    # all sambamba
+    time /tools/bwa-0.7.17/bwa mem ${BWA_OPTION} -R "${read_group}" ${REFERENCE} ${INPUT_FASTQ1} ${INPUT_FASTQ2} \
+        | /tools/sambamba view ${SAMBAMBA_VIEW_OPTION} /dev/stdin \
+        | /tools/sambamba sort ${SAMBAMBA_SORT_OPTION} -o ${output_file} /dev/stdin
+
+
+elif [ "${TOOL_COMBINATION}" == "3" ]; then
+    # samtools view and sambamba sort
+    time /tools/bwa-0.7.17/bwa mem ${BWA_OPTION} -R "${read_group}" ${REFERENCE} ${INPUT_FASTQ1} ${INPUT_FASTQ2} \
+        | /tools/samtools-1.9/samtools view ${SAMTOOLS_VIEW_OPTION} \
+        | /tools/sambamba sort ${SAMBAMBA_SORT_OPTION} -o ${output_file} /dev/stdin
+
+
+elif [ "${TOOL_COMBINATION}" == "4" ]; then
+    # biobambam sort
+    time /tools/bwa-0.7.17/bwa mem ${BWA_OPTION} -R "${read_group}" ${REFERENCE} ${INPUT_FASTQ1} ${INPUT_FASTQ2} \
+        | /usr/local/bin/bamsort ${BIOBAMBAM_OPTION} O=${output_file}
+
+
+fi
+
+samtools view ${output_file} | head -10
+
+ls -lh ${OUTPUT_DIR}
